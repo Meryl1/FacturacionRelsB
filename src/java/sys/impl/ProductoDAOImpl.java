28494ab513624223e6b2d58b68acde9c63c622be
Meryli <Meryli@DESKTOP-T5N391A>
2023-09-16 18:08:03 -0500

    correcciones finales

--- src/java/sys/impl/ProductoDAOImpl.java
+++ src/java/sys/impl/ProductoDAOImpl.java
@@ -1,4 +1,3 @@
-
 package sys.impl;
 
 import java.util.List;
@@ -8,72 +7,75 @@ import sys.dao.ProductoDAO;
 import sys.modelo.Producto;
 import sys.util.HibernateUtil;
 
-public class ProductoDAOImpl implements ProductoDAO{
+public class ProductoDAOImpl implements ProductoDAO {
 
     @Override
     public List<Producto> select() {
-        List<Producto> productos=null;
-        Session session=HibernateUtil.getSessionFactory().openSession();
-        Transaction trans=session.beginTransaction();
-        String hql= "FROM Producto";
-        try{
-            productos=session.createQuery(hql).list();
+        List<Producto> productos = null;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction trans = session.beginTransaction();
+        String hql = "FROM Producto";
+        try {
+            productos = session.createQuery(hql).list();
             trans.commit();
-        }catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
             trans.rollback();
-        }finally{
-          session.close();  
+        } finally {
+            session.close();
         }
-        return productos; 
+        return productos;
     }
 
     @Override
     public void insert(Producto producto) {
-        Session session=null;
-         try{
-            session=HibernateUtil.getSessionFactory().openSession();
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
             session.beginTransaction();
             session.save(producto);
             session.getTransaction().commit();
-        }catch (Exception ex){
+        } catch (Exception ex) {
             session.getTransaction().rollback();
-        }finally{
-            session.close(); 
-         }
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void update(Producto producto) {
-        Session session=null;
-        try{
-           session=HibernateUtil.getSessionFactory().openSession();
-           session.beginTransaction();
-           session.update(producto);
-           session.getTransaction().commit();
-        }catch (Exception ex){
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.update(producto);
+            session.getTransaction().commit();
+        } catch (Exception ex) {
             ex.printStackTrace();
             session.getTransaction().rollback();
-        }finally{
-           session.close();
+        } finally {
+            session.close();
         }
     }
 
     @Override
     public void delete(Producto producto) {
-        Session session=null;
-        try{
-        session=HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        session.delete(producto);
-        session.getTransaction();
-        }catch (Exception ex){
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.delete(producto);
+            session.getTransaction().commit();
+        } catch (Exception ex) {
             ex.printStackTrace();
-            session.getTransaction().rollback();
-        }finally{
-            session.close();
+            if (session != null && session.getTransaction() != null) {
+                session.getTransaction().rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
         }
     }
-    
-    
+
 }
